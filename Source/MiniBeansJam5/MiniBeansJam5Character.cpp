b1eadf64c00d38b12dac107076bd4eea29796e15
Alex <m.janssen@hotmail.de>
2020-04-03 22:41:57 +0200

    Anpassung Sprite an Camera Winkel

--- Source/MiniBeansJam5/MiniBeansJam5Character.cpp
+++ Source/MiniBeansJam5/MiniBeansJam5Character.cpp
@@ -5,6 +5,7 @@
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
+#include "PaperFlipbookComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -27,15 +28,12 @@ AMiniBeansJam5Character::AMiniBeansJam5Character()
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->bOrientRotationToMovement = false; // Character moves in the direction of input...	
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
-
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+	GetSprite()->SetWorldRotation(FRotator(-50.0, 0.0 , 90.0));
 }
 
 //////////////////////////////////////////////////////////////////////////
